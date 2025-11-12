a87e7920945cc5dd714a2fe50687435cb14863dd
Thorbeorn <52549642+thorbeorn@users.noreply.github.com>
2025-11-12 11:10:18 +0100

    Delete Bulk directory

--- Bulk/init.sh
+++
@@ -1,4 +0,0 @@
-#A executer pour restaurer la base de donnees dans le container mongo quand il est vide (premier demarrage ou perte du volume)
-docker exec -it mongodb bash
-mongorestore --gzip --archive=/openfoodfacts-mongodbdump.gz
-exit
