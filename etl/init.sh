33a7889c4cb10a3a139d803ccf2c19e000761c23
Thorbeorn <dylanllodrapro@gmail.com>
2025-11-17 14:30:32 +0100

    java rebuild

--- etl/init.sh
+++
@@ -1,4 +0,0 @@
-#A executer pour restaurer la base de donnees dans le container mongo quand il est vide (premier demarrage ou perte du volume)
-docker exec -it mongo bash
-mongorestore --gzip --archive=/openfoodfacts-mongodbdump.gz
-exit
