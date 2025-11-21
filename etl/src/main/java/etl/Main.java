5124ae83be7a6131428da6a752f2c114730528a2
Thorbeorn <dylanllodrapro@gmail.com>
2025-11-21 17:08:09 +0100

    debug v3

--- etl/src/main/java/etl/Main.java
+++ etl/src/main/java/etl/Main.java
@@ -11,22 +11,18 @@ public class Main {
 	    SparkSession sparkSession = Utils.initializeSparkSession("openfoodfacts-etl");
 	    
 	    Dataset<Row> dfProducts = Extractors.extractFromCSV(sparkSession, Constants.DATA_FILE_PRODUCTS, "\t");
-	    //Dataset<Row> dfDiets = Extractors.extractFromCSV(sparkSession, Constants.DATA_FILE_DIET, ",");
-	    //Dataset<Row> dfUsers = Extractors.extractFromCSV(sparkSession, Constants.DATA_FILE_USERS, ",");
-	    System.out.println("====================product ======================");
-	    dfProducts.show(20, false);
+	    Dataset<Row> dfDiets = Extractors.extractFromCSV(sparkSession, Constants.DATA_FILE_DIET, ",");
+	    Dataset<Row> dfUsers = Extractors.extractFromCSV(sparkSession, Constants.DATA_FILE_USERS, ",");
 	    
 	    Dataset<Row> dfProductsCleaned = Transformers.cleanProductData(dfProducts, sparkSession);
-	    //Dataset<Row> dfDietsCleaned = Transformers.cleanDietData(dfDiets, sparkSession);
-	    //Dataset<Row> dfUsersCleaned = Transformers.cleanUserData(dfUsers, dfDietsCleaned, sparkSession);
-	    System.out.println("====================product ======================");
-	    dfProductsCleaned.show(20, false);
+	    Dataset<Row> dfDietsCleaned = Transformers.cleanDietData(dfDiets, sparkSession);
+	    Dataset<Row> dfUsersCleaned = Transformers.cleanUserData(dfUsers, dfDietsCleaned, sparkSession);
 	    
-	    //Dataset<Row> weeklyMenus = Generators.generateWeeklyMenu(sparkSession, dfUsersCleaned, dfDietsCleaned, dfProductsCleaned);
+	    Dataset<Row> weeklyMenus = Generators.generateWeeklyMenu(sparkSession, dfUsersCleaned, dfDietsCleaned, dfProductsCleaned);
 	    
-	   // Loaders.loadToDatabase(dfDietsCleaned, Constants.DB_HOST, Constants.DB_USER, Constants.DB_PASSWORD, "regime");
-	   // Loaders.loadToDatabase(dfUsersCleaned, Constants.DB_HOST, Constants.DB_USER, Constants.DB_PASSWORD, "user");
-	   // Loaders.loadToDatabase(weeklyMenus, Constants.DB_HOST, Constants.DB_USER, Constants.DB_PASSWORD, "daily_menu");
+	    Loaders.loadToDatabase(dfDietsCleaned, Constants.DB_HOST, Constants.DB_USER, Constants.DB_PASSWORD, "regime");
+	    Loaders.loadToDatabase(dfUsersCleaned, Constants.DB_HOST, Constants.DB_USER, Constants.DB_PASSWORD, "user");
+	    Loaders.loadToDatabase(weeklyMenus, Constants.DB_HOST, Constants.DB_USER, Constants.DB_PASSWORD, "daily_menu");
 	    
 	    sparkSession.stop();
 	}
